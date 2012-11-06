af2a87dd6d0a6182cdaefcd54230fbd8ed25b50b
Yulia Kovaleva <ykovaleva@xiag.ch>
2012-11-06 12:52:51 +0700

    Added to Monitor: checking temp is writable.

--- src/Barberry/Plugin/Videocapture/Monitor.php
+++ src/Barberry/Plugin/Videocapture/Monitor.php
@@ -5,6 +5,13 @@ use Barberry\Plugin;
 
 class Monitor implements Plugin\InterfaceMonitor
 {
+    private $tempDir;
+
+    public function __construct($tempDir = '')
+    {
+        $this->tempDir = $tempDir;
+    }
+
     /**
      * @return array of error messages
      */
@@ -18,18 +25,24 @@ class Monitor implements Plugin\InterfaceMonitor
     }
 
     /**
-     * @return array of error messages
-     */
-    public function reportMalfunction()
-    {
-        // TODO: Implement reportMalfunction() method.
-    }
-
-    /**
      * @return bool whether ffmpeg is installed
      */
     protected function ffmpegIsInstalled()
     {
         return preg_match('/^\/\w+/', exec("which ffmpeg")) ? true : false;
+    }
+
+    /**
+     * @return array of error messages
+     */
+    public function reportMalfunction()
+    {
+        $report = $this->reportDirectoryIsWritable($this->tempDir);
+        return (!is_null($report)) ? array($report) : array();
+    }
+
+    private function reportDirectoryIsWritable($dir)
+    {
+        return (is_writable($dir)) ? null : 'ERROR: Temporary directory is not writable (Videocapture plugin)';
     }
 }
--- test/MonitorTest.php
+++ test/MonitorTest.php
@@ -3,6 +3,25 @@ namespace Barberry\Plugin\Videocapture;
 
 class MonitorTest extends \PHPUnit_Framework_TestCase
 {
+    private $testDirWritable;
+    private $testDirNotWritable;
+
+    protected function setUp()
+    {
+        $path = realpath(__DIR__) . '/../tmp';
+
+        $this->testDirWritable = $path . '/testdir-writable/';
+        $this->testDirNotWritable = $path . '/testdir-notwritable/';
+
+        @mkdir($this->testDirWritable, 0777, true);
+        @mkdir($this->testDirNotWritable, 0444, true);
+    }
+
+    protected function tearDown()
+    {
+        exec('rm -rf ' . $this->testDirWritable);
+        exec('rm -rf ' . $this->testDirNotWritable);
+    }
     public function testReportsIfFfimpegIsNotInstalled()
     {
         $monitor = $this->getMock('Barberry\\Plugin\\Videocapture\\Monitor', array('ffmpegIsInstalled'));
@@ -11,6 +30,23 @@ class MonitorTest extends \PHPUnit_Framework_TestCase
         $this->assertContains(
             'Please install ffmpeg',
             $monitor->reportUnmetDependencies()
+        );
+    }
+
+    public function testReportsNoErrorsIfDirectoryIsWritable()
+    {
+        $monitor = new Monitor($this->testDirWritable);
+        $this->assertEquals(array(), $monitor->reportMalfunction());
+    }
+
+    public function testReportsErrorsIfDirectoryIsNotWritable()
+    {
+        $monitor = new Monitor($this->testDirNotWritable);
+        $this->assertEquals(
+            array(
+                'ERROR: Temporary directory is not writable (Videocapture plugin)'
+            ),
+            $monitor->reportMalfunction()
         );
     }
 }
