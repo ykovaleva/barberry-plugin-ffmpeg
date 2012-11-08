f66d1013988af06feec41c77e2b9ce2dd15967fe
Yulia Kovaleva <ykovaleva@xiag.ch>
2012-11-08 15:56:07 +0700

    Added new parameters to Command

--- src/Barberry/Plugin/Videocapture/Command.php
+++ src/Barberry/Plugin/Videocapture/Command.php
@@ -1,19 +1,37 @@
 <?php
+/**
+ * Command class file, explodes input string to parameters for ffmpeg.
+ *
+ * Available command string (any parameter may be omitted, but their order is important):
+ * ab128_ac:mp3_vb256_vc:h263_r1_10~100x100
+ *
+ * ab128 - audio bitrate
+ * ac:mp3 - audio codec
+ * vb256 - video bitrate
+ * vc:h263 - video codec
+ * r1 - rotation parameter (
+ *     available values:
+ *     0 - default position. 1 - rotate 90 degree, 2 - 180 degree, 3 - 270 degree
+ * )
+ * last numeric parameter (10) - on what second make screenshot.
+ * Anything after '~' sign will be send to imagemagick barberry plugin as command string.
+ */
+
 namespace Barberry\Plugin\Videocapture;
 
 use Barberry\Plugin;
 
 class Command implements Plugin\InterfaceCommand
 {
-    const MAX_WIDTH = 1280; // output resolution either video or image
-    const MAX_HEIGHT = 720;
     const MAX_AUDIO_BITRATE = 256;
     const MAX_VIDEO_BITRATE = 4000;
+    const MAX_ROTATION = 3;
 
-    private $width;
-    private $height;
     private $audioBitrate;
+    private $audioCodec;
     private $videoBitrate;
+    private $videoCodec;
+    private $rotation;
     private $screenshotTime;
     private $commandForImagemagick;
 
@@ -27,15 +45,20 @@ class Command implements Plugin\InterfaceCommand
         $params = explode('_', $commands[0]);
 
         foreach ($params as $parameter) {
-            if (preg_match('/^([\d]*)x([\d]*)$/', $parameter, $matches)) {
-                $this->width = strlen($matches[1]) ? (int)$matches[1] : null;
-                $this->height = strlen($matches[2]) ? (int)$matches[2] : null;
-            }
-            if (preg_match('/^a([\d]+)$/', $parameter, $matches)) {
+            if (preg_match('/^ab([\d]+)$/', $parameter, $matches)) {
                 $this->audioBitrate = strlen($matches[1]) ? (int)$matches[1] : null;
             }
-            if (preg_match('/^v([\d]+)$/', $parameter, $matches)) {
+            if (preg_match('/^ac:([\w]+)$/', $parameter, $matches)) {
+                $this->audioCodec = strlen($matches[1]) ? $matches[1] : null;;
+            }
+            if (preg_match('/^vb([\d]+)$/', $parameter, $matches)) {
                 $this->videoBitrate = strlen($matches[1]) ? (int)$matches[1] : null;;
+            }
+            if (preg_match('/^vc:([\w]+)$/', $parameter, $matches)) {
+                $this->videoCodec = strlen($matches[1]) ? $matches[1] : null;;
+            }
+            if (preg_match('/^r([\d]+)$/', $parameter, $matches)) {
+                $this->rotation = strlen($matches[1]) ? (int)$matches[1] : null;;
             }
             if (preg_match('/^([\d]+)$/', $parameter, $matches)) {
                 $this->screenshotTime = (int)$matches[1];
@@ -58,24 +81,29 @@ class Command implements Plugin\InterfaceCommand
         return strval($this) === $commandString;
     }
 
-    public function width()
-    {
-        return min($this->width, self::MAX_WIDTH);
-    }
-
-    public function height()
-    {
-        return min($this->height, self::MAX_HEIGHT);
-    }
-
     public function audioBitrate()
     {
         return min($this->audioBitrate, self::MAX_AUDIO_BITRATE);
     }
 
+    public function audioCodec()
+    {
+        return is_null($this->audioCodec) ? null : $this->audioCodec;
+    }
+
     public function videoBitrate()
     {
         return min($this->videoBitrate, self::MAX_VIDEO_BITRATE);
+    }
+
+    public function videoCodec()
+    {
+        return is_null($this->videoCodec) ? null : $this->videoCodec;
+    }
+
+    public function rotation()
+    {
+        return min($this->rotation, self::MAX_ROTATION);
     }
 
     public function screenshotTime()
@@ -92,14 +120,20 @@ class Command implements Plugin\InterfaceCommand
     {
         $params = array();
 
-        if ($this->width || $this->height) {
-            $params[] = $this->width . 'x' . $this->height;
-        }
         if ($this->audioBitrate) {
-            $params[] = 'a' . $this->audioBitrate;
+            $params[] = 'ab' . $this->audioBitrate;
+        }
+        if ($this->audioCodec) {
+            $params[] = 'ac:' . $this->audioCodec;
         }
         if ($this->videoBitrate) {
-            $params[] = 'v' . $this->videoBitrate;
+            $params[] = 'vb' . $this->videoBitrate;
+        }
+        if ($this->videoCodec) {
+            $params[] = 'vc:' . $this->videoCodec;
+        }
+        if ($this->rotation) {
+            $params[] = 'r' . $this->rotation;
         }
         if ($this->screenshotTime) {
             $params[] = $this->screenshotTime;
--- test/CommandTest.php
+++ test/CommandTest.php
@@ -5,22 +5,38 @@ class CommandTest extends \PHPUnit_Framework_TestCase
 {
     public function testThereAreNoParametersByDefault()
     {
-        $this->assertNull(self::command()->width());
-        $this->assertNull(self::command()->height());
+        $this->assertNull(self::command()->audioCodec());
         $this->assertNull(self::command()->audioBitrate());
+        $this->assertNull(self::command()->videoCodec());
         $this->assertNull(self::command()->videoBitrate());
+        $this->assertNull(self::command()->rotation());
         $this->assertNull(self::command()->screenshotTime());
         $this->assertNull(self::command()->commandForImageMagick());
     }
 
-    public function testExtractsVideoBitrateParameter()
-    {
-        $this->assertEquals(256, self::command('v256~50x50')->videoBitrate());
-    }
-
     public function testExtractsAudioBitrateParameter()
     {
-        $this->assertEquals(128, self::command('a128_v256~50x50')->audioBitrate());
+        $this->assertEquals(128, self::command('ab128_vb256~50x50')->audioBitrate());
+    }
+
+    public function testExtractsAudioCodecParameter()
+    {
+        $this->assertEquals('aac', self::command('ab128_ac:aac_vb256~50x50')->audioCodec());
+    }
+
+    public function testExtractsVideoBitrateParameter()
+    {
+        $this->assertEquals(256, self::command('vb256~50x50')->videoBitrate());
+    }
+
+    public function testExtractsVideoCodecParameter()
+    {
+        $this->assertEquals('h263', self::command('ab128_vc:h263_vb256~50x50')->videoCodec());
+    }
+
+    public function testExtractsRotationParameter()
+    {
+        $this->assertEquals(2, self::command('r2_vb256~50x50')->rotation());
     }
 
     public function testExtractsScreenshotTime()
@@ -28,34 +44,23 @@ class CommandTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(135, self::command('135_v256~50x50')->screenshotTime());
     }
 
-    public function testExtractsWidthParameter()
-    {
-        $this->assertEquals(250, self::command('250x_135_v256~50x50')->width());
-    }
-
-    public function testExtractsHeightParameter()
-    {
-        $this->assertEquals(150, self::command('x150_135_v256~50x50')->height());
-    }
-
     public function testExtractsImageMagickCommand()
     {
-        $this->assertEquals('800x600', self::command('x150_135_v256~800x600')->commandForImageMagick());
+        $this->assertEquals('800x600', self::command('135_vb256~800x600')->commandForImageMagick());
     }
 
     public function testTransformsParamToMaximumAvailableValues()
     {
-        $command = self::command('9000x9000_a512_v4500');
-        $this->assertEquals(1280, $command->width());
-        $this->assertEquals(720, $command->height());
+        $command = self::command('9000x9000_ab512_vb4500_r180');
         $this->assertEquals(256, $command->audioBitrate());
         $this->assertEquals(4000, $command->videoBitrate());
+        $this->assertEquals(3, $command->rotation());
     }
 
     public function testAmbiguityCommand()
     {
         $this->assertFalse(
-            self::command('250x250p_va128_a256_123456_~25x25')->conforms('250x250_v128_a256_1234567890~25x25')
+            self::command('va128_a256_123456_~25x25')->conforms('vb128_ab256_123456~25x25')
         );
     }
 
@@ -70,10 +75,10 @@ class CommandTest extends \PHPUnit_Framework_TestCase
     public static function correctCommands()
     {
         return array(
-            array('500x500_a128_v256_135~100x100'),
-            array('200x_a128_v256'),
+            array('ab128_vb256_135~100x100'),
+            array('ab128_ac:mp3_vb512_vc:h263_10'),
             array('~100x100'),
-            array('a128_1351843630~100x200')
+            array('ab128_135~100x200')
         );
     }
 
